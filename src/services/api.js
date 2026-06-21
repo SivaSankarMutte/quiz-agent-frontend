3ad7fdb4fb57000453854cc96ae2c9bfa4539484
Siva <sivasankarmutte@gmail.com>
2026-06-21 13:16:40 +0530

    Modified the .env APP url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  // baseURL: "http://localhost:8000"  // for Local development
+  baseURL: import.meta.env.VITE_API_URL // for Production environment
 });
 
 api.interceptors.request.use(
